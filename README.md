f9ccfcc309af4151222a6ecf572e0eb9a0383d0f
Kristen <killingsworth_kristen@yahoo.com>
2022-10-07 13:05:38 -0400

    finsihed js functionality specifically sending/retrieving local storage

---
+++ README.md
@@ -0,0 +1,5 @@
+<!-- # Work Scheduler
+This application allows you to see your work day (9AM-5PM) and plan tasks in those hour blocks
+
+## installation
+There -->
---
+++ script.js
@@ -0,0 +1,136 @@
+// declare global variables
+var timeCompare = 9;
+var eventCompare = 9;
+var eventArr = [];
+
+getLocal()
+
+// Sets date inside the header of the page
+var now = moment().format('MMMM do, YYYY');
+$("#currentDay").text(`${now}`);
+
+// function that checks current time against time blocks
+//      if current time is > block turn it gray
+//      if current time = block turn red
+function changeBlockColor () {
+    let currentTime = moment().hours()
+    while (currentTime > timeCompare) {
+        $(`#${timeCompare}`).css("background","gray")
+        timeCompare++;
+        if (timeCompare > 17) {
+            return;
+        }
+    }
+    if (currentTime === timeCompare) {
+        $(`#${timeCompare}`).css("background","orange")
+    }
+
+}
+changeBlockColor();
+
+// function that adds the correct text input into the correct object in the eventArr
+function textInput () {
+    for (i = 0; i <9; i++) {
+        
+        eventCompare++;
+        if (eventCompare > 17) {
+            return;
+        }
+        console.log(eventArr);
+    }
+}
+
+// gets local storage for all blocks on page load
+function getLocal () {
+    var user = localStorage.getItem(`task-9`)
+    $(`.task-input-9`).append(user);
+    var user = localStorage.getItem(`task-10`)
+    $(`.task-input-10`).append(user);
+    var user = localStorage.getItem(`task-11`)
+    $(`.task-input-11`).append(user);
+    var user = localStorage.getItem(`task-12`)
+    $(`.task-input-12`).append(user);
+    var user = localStorage.getItem(`task-13`)
+    $(`.task-input-13`).append(user);
+    var user = localStorage.getItem(`task-14`)
+    $(`.task-input-14`).append(user);
+    var user = localStorage.getItem(`task-15`)
+    $(`.task-input-15`).append(user);
+    var user = localStorage.getItem(`task-16`)
+    $(`.task-input-16`).append(user);
+    var user = localStorage.getItem(`task-17`)
+    $(`.task-input-17`).append(user);
+}
+// save button puts text in taskBlock into local storage/pulls from local storage
+$(`.save-block-9`).on("click", function(event){
+    event.preventDefault();
+    var user = localStorage.getItem(`task-9`)
+    $(`.task-input-9`).append(user);
+    var task9 = $(`.task-input-9`).val();
+    localStorage.setItem(`task-9`, task9);
+})
+
+$(`.save-block-10`).on("click", function(event){
+    event.preventDefault();
+    var user = localStorage.getItem(`task-10`)
+    $(`.task-input-10`).append(user);
+    var task9 = $(`.task-input-10`).val();
+    localStorage.setItem(`task-10`, task9);
+})
+
+$(`.save-block-11`).on("click", function(event){
+    event.preventDefault();
+    var user = localStorage.getItem(`task-11`)
+    $(`.task-input-11`).append(user);
+    var task9 = $(`.task-input-11`).val();
+    localStorage.setItem(`task-11`, task9);
+})
+
+$(`.save-block-12`).on("click", function(event){
+    event.preventDefault();
+    var user = localStorage.getItem(`task-12`)
+    $(`.task-input-12`).append(user);
+    var task9 = $(`.task-input-12`).val();
+    localStorage.setItem(`task-12`, task9);
+})
+
+$(`.save-block-13`).on("click", function(event){
+    event.preventDefault();
+    var user = localStorage.getItem(`task-13`)
+    $(`.task-input-13`).append(user);
+    var task9 = $(`.task-input-13`).val();
+    localStorage.setItem(`task-13`, task9);
+})
+
+$(`.save-block-14`).on("click", function(event){
+    event.preventDefault();
+    var user = localStorage.getItem(`task-14`)
+    $(`.task-input-14`).append(user);
+    var task9 = $(`.task-input-14`).val();
+    localStorage.setItem(`task-14`, task9);
+})
+
+$(`.save-block-15`).on("click", function(event){
+    event.preventDefault();
+    var user = localStorage.getItem(`task-15`)
+    $(`.task-input-15`).append(user);
+    var task9 = $(`.task-input-15`).val();
+    localStorage.setItem(`task-15`, task9);
+})
+
+$(`.save-block-16`).on("click", function(event){
+    event.preventDefault();
+    var user = localStorage.getItem(`task-16`)
+    $(`.task-input-16`).append(user);
+    var task9 = $(`.task-input-16`).val();
+    localStorage.setItem(`task-16`, task9);
+})
+
+$(`.save-block-17`).on("click", function(event){
+    event.preventDefault();
+    var user = localStorage.getItem(`task-17`)
+    $(`.task-input-17`).append(user);
+    var task9 = $(`.task-input-17`).val();
+    localStorage.setItem(`task-17`, task9);
+})
+
